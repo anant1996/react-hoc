a8f9434dfee50b202b8661b20890ee8f442e6ab6
anant1996 <56394549+anant1996@users.noreply.github.com>
2021-01-04 17:36:52 +0530

    second changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './styles.module.css'
-import Button from "./components/Button";
+// import Button from "./components/Button"
 
-export {
-  Button
+export const Button = ({ label }) => {
+  return <button className={styles.test}>{label}</button>
 }
